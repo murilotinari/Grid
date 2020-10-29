856d9398eeca9158f01747b550b8fcce8019aa64
murilotinari <murilotinari@outlook.com>
2020-10-28 23:16:16 -0300

    Versao 1

---
+++ Grid/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Grid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(616, 57);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(110, 86);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.AllowUserToOrderColumns = true;
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.DataGridView.Location = new System.Drawing.Point(49, 183);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.Size = new System.Drawing.Size(958, 378);
+            this.DataGridView.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "";
+            this.Column1.Name = "Column1";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(114, 57);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(113, 20);
+            this.lbl1.TabIndex = 4;
+            this.lbl1.Text = "f(x) = x^2 +x - 7";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(356, 57);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(44, 20);
+            this.lblA.TabIndex = 5;
+            this.lblA.Text = "a = 3";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(356, 90);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(49, 20);
+            this.lblB.TabIndex = 6;
+            this.lblB.Text = "b = -3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 650);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.btnCalcular);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        public System.Windows.Forms.DataGridView DataGridView;
+    }
+}
+
---
+++ Grid/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataTable Tbl;
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Tbl = new DataTable();
+
+            Tbl.Columns.Add("Rodada", typeof(double));
+            Tbl.Columns.Add("a", typeof(double));
+            Tbl.Columns.Add("f(a)", typeof(double));
+            Tbl.Columns.Add("b", typeof(double));
+            Tbl.Columns.Add("f(b)", typeof(double));
+            Tbl.Columns.Add("Xk", typeof(double));
+            Tbl.Columns.Add("f(Xk)", typeof(double));
+            Tbl.Columns.Add("Precisao", typeof(double));
+
+            DataRow Linha;
+
+
+            int x = 1;
+            double[] valorA = new double[x], valorB = new double[x], funcaoDeA = new double[x], funcaoDeB = new double[x], valorXk = new double[x], funcaoDeXk = new double[x];
+            double[] rodadas = new double[x], precisao = new double[x];
+            double E = 0.1;
+            lblA.Text = "3";
+            lblB.Text = "-3";
+            
+
+            valorA[0] = double.Parse(lblA.Text);
+            valorB[0] = double.Parse(lblB.Text);
+
+            funcaoDeA[0] = Math.Pow(double.Parse(lblA.Text), 2) + double.Parse(lblA.Text) - 7;
+            funcaoDeB[0] = Math.Pow(double.Parse(lblB.Text), 2) + double.Parse(lblB.Text) - 7;
+
+            valorXk[0] = (valorA[0] + valorB[0])/2;
+            
+            funcaoDeXk[0] = Math.Pow(valorXk[0], 2) + valorXk[0] - 7;
+            rodadas[0] = 0;
+            for (x = 1; x > -1; x++)
+            {                         
+
+                precisao[x - 1] = valorXk[x - 1] - valorA[x - 1];
+
+                if (precisao[x - 1] < 0)
+                    precisao[x - 1] = - precisao[x - 1];
+
+
+                if (precisao[x - 1] < E)
+                    break;
+
+                Array.Resize(ref valorA, x + 1);
+                Array.Resize(ref valorB, x + 1);
+                Array.Resize(ref funcaoDeA , x + 1);
+                Array.Resize(ref funcaoDeB , x + 1);
+                Array.Resize(ref valorXk , x + 1);
+                Array.Resize(ref funcaoDeXk , x + 1);
+                Array.Resize(ref rodadas, x + 1);
+                Array.Resize(ref precisao, x + 1);
+
+
+                if (funcaoDeXk[x - 1] > 0)
+                {
+                    valorA[x] = valorXk[x - 1];
+                    valorB[x] = valorB[x - 1];
+                }
+                else if (funcaoDeXk[x - 1] < 0)
+                {
+                    valorB[x] = valorXk[x - 1];
+                    valorA[x] = valorA[x - 1];
+                }
+                else
+                    break;
+
+                funcaoDeA[x] = equacao(valorA[x]);
+                funcaoDeB[x] = equacao(valorB[x]);
+
+                valorXk[x] = (valorA[x] + valorB[x]) / 2;
+                funcaoDeXk[x] = equacao(valorXk[x]);
+
+                rodadas[x] = x;
+
+                Linha = Tbl.NewRow();
+
+                Linha[0] = Math.Round(rodadas[x - 1], 7);
+                Linha["a"] = Math.Round(valorA[x - 1], 7);
+                Linha["f(a)"] = Math.Round(funcaoDeA[x - 1], 7);
+                Linha["b"] = Math.Round(valorB[x - 1], 7);
+                Linha["f(b)"] = Math.Round(funcaoDeB[x - 1], 7);
+                Linha["Xk"] = Math.Round(valorXk[x - 1], 7);
+                Linha["f(Xk)"] = Math.Round(funcaoDeXk[x - 1], 7);
+                Linha["Precisao"] = Math.Round(precisao[x - 1], 7);
+
+                Tbl.Rows.Add(Linha);
+            }
+
+                       
+            DataGridView.DataSource = Tbl;
+
+
+        }
+        static double equacao(double entrada)
+        {
+            return (Math.Pow(entrada, 2) + entrada - 7);
+        }
+    }
+}
